e32fec5cf1d7317f2cc6f6a1d96736ebc99e1361
Propheteemmas243-cmyk <propheteemmas243@gmail.com>
2026-07-02 21:51:38 +0100

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWqqE6-AXGtaR0Gk--x4aYQTwEEcZ2hVY",
+  authDomain: "prayer-room-c9232.firebaseapp.com",
+  projectId: "prayer-room-c9232",
+  storageBucket: "prayer-room-c9232.firebasestorage.app",
+  messagingSenderId: "637664120720",
+  appId: "1:637664120720:web:88df3f60aca44d2bdcad15"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const firebaseConfig = {
   apiKey: "AIzaSyBWqqE6-AXGtaR0Gk--x4aYQTwEEcZ2hVY",
   authDomain: "prayer-room-c9232.firebaseapp.com",
@@ -35,3 +49,14 @@ document.getElementById("prayer").innerText = randomItem(prayers);
 function openLive() {
   window.open("https://youtube.com", "_blank");
 }
+
+async function loadVerse() {
+  const docRef = doc(db, "content", "verse");
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    document.getElementById("verse").innerText = docSnap.data().text;
+  }
+}
+
+loadVerse();
