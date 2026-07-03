928bf1bf93036d93427dc1281ee5038e44a3692d
Propheteemmas243-cmyk <propheteemmas243@gmail.com>
2026-07-03 18:46:08 +0100

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-// Configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBWqqE6-AXGtaR0Gk--x4aYQTwEEcZ2hVY",
   authDomain: "prayer-room-c9232.firebaseapp.com",
@@ -11,14 +10,12 @@ const firebaseConfig = {
   appId: "1:637664120720:web:88df3f60aca44d2bdcad15"
 };
 
-// Initialisation
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Charger les données
 async function chargerContenu() {
   try {
-    const ref = doc(db, "Contenu", "principal");
+    const ref = doc(db, "app", "principal");
     const snap = await getDoc(ref);
 
     if (snap.exists()) {
@@ -34,16 +31,16 @@ async function chargerContenu() {
       document.getElementById("verse").innerText = "Document introuvable";
       document.getElementById("prayer").innerText = "Document introuvable";
     }
+
   } catch (error) {
     console.error(error);
-    document.getElementById("verse").innerText = "Erreur de connexion";
-    document.getElementById("prayer").innerText = "Erreur de connexion";
+    document.getElementById("verse").innerText = "Erreur";
+    document.getElementById("prayer").innerText = "Erreur";
   }
 }
 
 chargerContenu();
 
-// Bouton Live
 window.openLive = function () {
   window.open("https://youtube.com", "_blank");
 };
